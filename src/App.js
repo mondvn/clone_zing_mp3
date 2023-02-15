58670dfe2ac1b68548494ce21b2981b19c9e095d
MondVn <trannamanh94@gmail.com>
2023-02-15 23:16:40 +0700

    setup axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+// import { useState } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
 
 import { Home, Public, Login } from './pages/public'
--- src/pages/public/Home.js
+++ src/pages/public/Home.js
@@ -1,6 +1,16 @@
-import Header from "../../components/Header";
+import { useEffect } from 'react';
+
+import * as apis from '../../apis'
 
 function Home() {
+  useEffect(() => {
+    const fetchDataHome = async () => {
+      const response = await apis.getHome()
+      console.log({ response })
+    }
+    fetchDataHome()
+  }, [])
+
   return (
     <div className="overflow-y-auto mt-[70px]">
       Home
