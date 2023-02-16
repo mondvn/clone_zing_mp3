5498fa96909de07d3687e272f2ecfac808130ce1
MondVn <trannamanh94@gmail.com>
2023-02-16 16:44:21 +0700

    setup config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'layout-bg': '#1E1E1E',
+        'slider-bg': '#292929',
+        'slider-hover-bg': '#404040',
+        'player-bg': '#181818',
+        'button-primary-bg': '#9B4DE0',
+        'button-bg': '#FFFFFF1A',
+        'input-bg': '#FFFFFF1A',
+      },
+      colors: {
+        'primary-color': '#FFFFFF',
+        'slider-color': '#a0a0a0',
+        'button-header-color': '#C273ED',
+      },
+      borderColor: {
+        'player-border-color': '#353535',
+      },
       keyframes: {
         'slide-right': {
           '0%': {
