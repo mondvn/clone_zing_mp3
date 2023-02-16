d89830da68d164e9e6891093aac29d1f5600187a
MondVn <trannamanh94@gmail.com>
2023-02-16 14:26:40 +0700

    add animation slder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,45 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-right': {
+          '0%': {
+            '-webkit-transform': 'translateX(-500px)',
+            'transform': 'translateX(-500px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            'transform': 'translateX(0)',
+          }
+        },
+        'slide-left': {
+          '0%': {
+            '-webkit-transform': 'translateX(500px)',
+            'transform': 'translateX(500px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            'transform': 'translateX(0)',
+          }
+        },
+        'slide-left-2': {
+          '0%': {
+            '-webkit-transform': 'translateX(500px)',
+            'transform': 'translateX(500px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            'transform': 'translateX(0)',
+          }
+        }
+      },
+      animation: {
+        'slide-right': 'slide-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;',
+        'slide-left': 'slide-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;',
+        'slide-left-2': 'slide-left-2 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;',
+      },
+    },
   },
   plugins: [],
 }
