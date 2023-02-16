0e0d13ee18e48c503f2d65e7a2fd4f4438bd8db6
MondVn <trannamanh94@gmail.com>
2023-02-16 23:04:06 +0700

    fix link logo and hover navbar

--- src/components/SidebarLeft.js
+++ src/components/SidebarLeft.js
@@ -3,17 +3,22 @@ import React from 'react'
 import logoDark from '../assets/images/logo-dark.svg'
 import liveTag from '../assets/images/live-tag.svg'
 import { sidebarMenu } from '../ultis/menu'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import path from '../ultis/path'
 
-const notActiveStyle = 'py-2 px-[25px] font-bold text-[13px] text-[#A0A0A0] flex justify-start items-center'
+const notActiveStyle = 'py-2 px-[25px] font-bold text-[13px] text-[#A0A0A0] flex justify-start items-center hover:text-primary-text-color'
 const activeStyle = 'py-2 px-[25px] font-bold text-[13px] text-[#FFFFFF] flex justify-start items-center bg-[hsla(0,0%,100%,0.1)] border-l-[3px] border-[#994cdd]'
 
 const SidebarLeft = () => {
+  const navigate = useNavigate()
+
   return (
     <div className='flex flex-col bg-[hsla(0,0%,100%,0.05)] h-full'>
-      <div className='w-full h-[70px] px-[28px] flex items-center justify-start'>
+      <Link
+        to={path.HOME}
+        className='w-full h-[70px] px-[28px] flex items-center justify-start cursor-pointer'>
         <img src={logoDark} alt='logo' className='w-[120px] h-10 object-contain' />
-      </div>
+      </Link>
       <div className='flex flex-col'>
         {sidebarMenu.map((item) => (
           <NavLink
